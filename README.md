99fa8858451c3469dfecc5a57a06443abef6ccaa
Craig Kelly <craig.n.kelly@gmail.com>
2015-04-28 18:34:52 -0500

    doc update

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 demo-survey
 ================
 
+This is a sample app demonstrating how to use Java and Amazon Web Services
+to create a web form whose results are saved to a database.
+
 See the latest documentation at http://memphis-iis.github.io/demo-survey
--- src/main/java/edu/memphis/iis/demosurvey/DataStoreClient.java
+++ src/main/java/edu/memphis/iis/demosurvey/DataStoreClient.java
@@ -47,14 +47,14 @@ import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
  * we use in saveSurvey).
  */
 public class DataStoreClient {
-	private final static Logger logger = LoggerFactory.getLogger(DataStoreClient.class);
+    private final static Logger logger = LoggerFactory.getLogger(DataStoreClient.class);
 
-	/**
-	 * The list of tables that should be insured (created if missing) on
-	 * startup. Note that if there is a class in this array that isn't
-	 * annotated with @DynamoDBTable with tableName specified you WILL
-	 * get exceptions on startup).
-	 * */
+    /**
+     * The list of tables that should be insured (created if missing) on
+     * startup. Note that if there is a class in this array that isn't
+     * annotated with @DynamoDBTable with tableName specified you WILL
+     * get exceptions on startup).
+     * */
     private final static Class<?>[] TABLES = {Survey.class};
 
     /** Default read capacity set for DynamoDB tables on creation */
@@ -63,174 +63,180 @@ public class DataStoreClient {
     /** Default write capacity set for DynamoDB tables on creation */
     private final static long DEFAULT_WRITE_CAPACITY = 5L;
 
-	/** Created and managed by constructor */
-	private AmazonDynamoDBClient client;
+    /** Created and managed by constructor */
+    private AmazonDynamoDBClient client;
 
-	/** Created and managed by helper function - shouldn't be used directly in code */
-	private DynamoDB db;
+    /** Created and managed by helper function - shouldn't be used directly in code */
+    private DynamoDB db;
 
-	/** Created and managed by helper function - shouldn't be used directly in code */
-	private DynamoDBMapper mapper;
+    /** Created and managed by helper function - shouldn't be used directly in code */
+    private DynamoDBMapper mapper;
 
-	/** Default constructor */
-	public DataStoreClient() {
-		// Note our lack of credentials - this is because in Elastic Beanstalk
-		// we will be specifying AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-		// via environment variables
-		client = new AmazonDynamoDBClient(new DefaultAWSCredentialsProviderChain());
+    /** Default constructor */
+    public DataStoreClient() {
+        // Note our lack of credentials - this is because in Elastic Beanstalk
+        // we will be specifying AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+        // via environment variables
+        client = new AmazonDynamoDBClient(new DefaultAWSCredentialsProviderChain());
 
-		String endpoint = System.getProperty("aws.dynamoEndpoint");
-		if (!Utils.isBlankString(endpoint)) {
-			//No env vars for security - we must be in testing
-			logger.info("Using specified endpoint {}", endpoint);
-			client.setEndpoint(endpoint);
-		}
+        String endpoint = System.getProperty("aws.dynamoEndpoint");
+        if (!Utils.isBlankString(endpoint)) {
+            //No env vars for security - we must be in testing
+            logger.info("Using specified endpoint {}", endpoint);
+            client.setEndpoint(endpoint);
+        }
 
-		//These will be lazy-init'ed by our helper functions
-		db = null;
-		mapper = null;
-	}
+        //These will be lazy-init'ed by our helper functions
+        db = null;
+        mapper = null;
+    }
 
-	/**
-	 * Simple helper to lazy-create the (low-level) Dynamo DB instance for
-	 * our current client.
-	 * @return valid instance of Dynamo DB
-	 */
-	protected DynamoDB getDB() {
-		if (db == null) {
-			db = new DynamoDB(client);
-		}
-		return db;
-	}
+    /**
+     * Simple helper to lazy-create the (low-level) Dynamo DB instance for
+     * our current client.
+     * @return valid instance of Dynamo DB
+     */
+    protected DynamoDB getDB() {
+        if (db == null) {
+            db = new DynamoDB(client);
+        }
+        return db;
+    }
 
-	/**
-	 * Simple helper to lazy-create the (high-level) Dynamo DB mapper instance
-	 * for our current client.
-	 * @return valid instance of Dynamo DB Mapper
-	 */
-	protected DynamoDBMapper getMapper() {
-		if (mapper == null) {
-			mapper = new DynamoDBMapper(client);
-		}
-		return mapper;
-	}
+    /**
+     * Simple helper to lazy-create the (high-level) Dynamo DB mapper instance
+     * for our current client.
+     * @return valid instance of Dynamo DB Mapper
+     */
+    protected DynamoDBMapper getMapper() {
+        if (mapper == null) {
+            mapper = new DynamoDBMapper(client);
+        }
+        return mapper;
+    }
 
-	/**
-	 * Insure that the current database schema is properly loaded.
-	 * This example pattern creates a set of tables needed, removes
-	 * any previously created tables from that set, and then creates
-	 * any tables left over.
-	 */
+    /**
+     * Insure that the current database schema is properly loaded.
+     * This example pattern creates a set of tables needed, removes
+     * any previously created tables from that set, and then creates
+     * any tables left over.
+     */
     public void ensureSchema() {
-		Map<String, Class<?>> tablesNeeded = new HashMap<>();
+        Map<String, Class<?>> tablesNeeded = new HashMap<>();
 
-		//Extract table names from table classes
-		for(Class<?> c: TABLES) {
-		    DynamoDBTable ann = (DynamoDBTable)c.getAnnotation(DynamoDBTable.class);
-		    tablesNeeded.put(ann.tableName(), c);
-		    logger.info("Table Configuration Found: " + ann.tableName());
-		}
+        //Extract table names from table classes
+        for(Class<?> c: TABLES) {
+            DynamoDBTable ann = (DynamoDBTable)c.getAnnotation(DynamoDBTable.class);
+            tablesNeeded.put(ann.tableName(), c);
+            logger.info("Table Configuration Found: " + ann.tableName());
+        }
 
-		for(Table t: getDB().listTables()) {
-			String tableName = t.getTableName();
-			logger.info("Existing Table Found: " + tableName);
-			tablesNeeded.remove(tableName);
-		}
+        for(Table t: getDB().listTables()) {
+            String tableName = t.getTableName();
+            logger.info("Existing Table Found: " + tableName);
+            tablesNeeded.remove(tableName);
+        }
 
-		//Create any tables we didn't find
-		for(String tableName: tablesNeeded.keySet()) {
-		    logger.info("CREATING Table: " + tableName);
+        //Create any tables we didn't find
+        //NOTE that we iterate through the keys and then access the value in
+        //the loop below. It would be more efficient to iterate over the
+        //EntrySet (which would contain both the key and the value). However,
+        //we leaving it this way so that there will be a warning to display in
+        //the FindBug's report.
+        for(String tableName: tablesNeeded.keySet()) {
+            logger.info("CREATING Table: " + tableName);
 
-		    Class<?> tableClass = tablesNeeded.get(tableName);
+            //FIND BUGS: this line should be flagged in the FindBugs report
+            Class<?> tableClass = tablesNeeded.get(tableName);
 
-		    db.createTable(getMapper()
-	            .generateCreateTableRequest(tableClass)
+            db.createTable(getMapper()
+                .generateCreateTableRequest(tableClass)
                 .withProvisionedThroughput(new ProvisionedThroughput()
                     .withReadCapacityUnits(DEFAULT_READ_CAPACITY)
                     .withWriteCapacityUnits(DEFAULT_WRITE_CAPACITY)
                 )
             );
-		}
-	}
+        }
+    }
 
-	/**
-	 * Persist the given survey
-	 *
-	 * @param survey the object to save
-	 * @param allowOverwrite if true, a previous record will overwritten
-	 */
-	public void saveSurvey(Survey survey, boolean allowOverwrite) {
-		if (survey == null || !survey.isValid()) {
-			throw new IllegalArgumentException("Invalid survey cannot be saved");
-		}
+    /**
+     * Persist the given survey
+     *
+     * @param survey the object to save
+     * @param allowOverwrite if true, a previous record will overwritten
+     */
+    public void saveSurvey(Survey survey, boolean allowOverwrite) {
+        if (survey == null || !survey.isValid()) {
+            throw new IllegalArgumentException("Invalid survey cannot be saved");
+        }
 
-		if (allowOverwrite) {
-		    //Just fire a save and completely overwrite the original record
-		    getMapper().save(
-		        survey,
-		        new DynamoDBMapperConfig(DynamoDBMapperConfig.SaveBehavior.CLOBBER)
-		    );
-		}
-		else {
-		    //Throw an exception if the record already exists
-		    getMapper().save(
-	            survey,
-	            new DynamoDBSaveExpression()
-	                .withExpected(expectKey(Survey.class))
-	        );
-		}
-	}
+        if (allowOverwrite) {
+            //Just fire a save and completely overwrite the original record
+            getMapper().save(
+                survey,
+                new DynamoDBMapperConfig(DynamoDBMapperConfig.SaveBehavior.CLOBBER)
+            );
+        }
+        else {
+            //Throw an exception if the record already exists
+            getMapper().save(
+                survey,
+                new DynamoDBSaveExpression()
+                    .withExpected(expectKey(Survey.class))
+            );
+        }
+    }
 
-	/**
-	 * Return a list of all surveys. Note that the underlying implementation
-	 * of the list is unspecified. Currently we use the AWS SDK's lazy-loading
-	 * list (which returns a page at a time). There may be delays while iterating
-	 * over this list, AND this may change in the future
-	 *
-	 * @return a List<> of Survey instances, or a List<> of size 0 if no
-	 *         Survey's are found
-	 */
-	public List<Survey> findSurveys() {
-	    return getMapper().scan(
+    /**
+     * Return a list of all surveys. Note that the underlying implementation
+     * of the list is unspecified. Currently we use the AWS SDK's lazy-loading
+     * list (which returns a page at a time). There may be delays while iterating
+     * over this list, AND this may change in the future
+     *
+     * @return a List<> of Survey instances, or a List<> of size 0 if no
+     *         Survey's are found
+     */
+    public List<Survey> findSurveys() {
+        return getMapper().scan(
             Survey.class,
             new DynamoDBScanExpression()
         );
-	}
+    }
 
-	/**
-	 * Given a "table" (the Class<?> for a class that is annotated with
-	 * DynamoDBTable), return the appropriate map. NOTE that if you don't
-	 * have a method annotated with DynamoDBHashKey and attributeName
-	 * specified, an exception will be thrown
-	 * @param table instance of Class<?>
-	 * @return a Map suitable for use as an Expected in a DynamoDBSaveExpression
-	 */
-	private Map<String, ExpectedAttributeValue> expectKey(Class<?> table) {
-	    String keyName = keyAttributeName(table);
-	    if (Utils.isBlankString(keyName)) {
-	        throw new IllegalArgumentException(table.getCanonicalName() + " has no DynamoDBHashKey specified");
-	    }
+    /**
+     * Given a "table" (the Class<?> for a class that is annotated with
+     * DynamoDBTable), return the appropriate map. NOTE that if you don't
+     * have a method annotated with DynamoDBHashKey and attributeName
+     * specified, an exception will be thrown
+     * @param table instance of Class<?>
+     * @return a Map suitable for use as an Expected in a DynamoDBSaveExpression
+     */
+    private Map<String, ExpectedAttributeValue> expectKey(Class<?> table) {
+        String keyName = keyAttributeName(table);
+        if (Utils.isBlankString(keyName)) {
+            throw new IllegalArgumentException(table.getCanonicalName() + " has no DynamoDBHashKey specified");
+        }
 
-	    Map<String, ExpectedAttributeValue> expected = new HashMap<>();
+        Map<String, ExpectedAttributeValue> expected = new HashMap<>();
         expected.put(keyName, new ExpectedAttributeValue(false));
         return expected;
-	}
+    }
 
-	/**
-	 * Given a table (the Class<?> for a class that is annotated with
+    /**
+     * Given a table (the Class<?> for a class that is annotated with
      * DynamoDBTable), return the attribute name of the key to the table
      * as specified by the DynamoDBHashKey annotation
-	 * @param table instance of Class<?> for a class annotated DynamoDBTable
-	 * @return the attribute name of the key for the table
-	 */
-	private String keyAttributeName(Class<?> table) {
-	    for(Method m: table.getMethods()) {
-	         Annotation keyAttr = m.getAnnotation(DynamoDBHashKey.class);
-	        if (keyAttr != null) {
-	            return ((DynamoDBHashKey)keyAttr).attributeName();
-	        }
-	    }
+     * @param table instance of Class<?> for a class annotated DynamoDBTable
+     * @return the attribute name of the key for the table
+     */
+    private String keyAttributeName(Class<?> table) {
+        for(Method m: table.getMethods()) {
+             Annotation keyAttr = m.getAnnotation(DynamoDBHashKey.class);
+            if (keyAttr != null) {
+                return ((DynamoDBHashKey)keyAttr).attributeName();
+            }
+        }
 
-	    return null; //Not found
-	}
+        return null; //Not found
+    }
 }
